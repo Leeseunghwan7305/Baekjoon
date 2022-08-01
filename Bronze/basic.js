894084fabacb46b8ad9beb5c60df14fb306f7e12
seunghwan <sun1238799@naver.com>
2022-08-01 22:03:13 +0900

    새싹

--- Bronze/basic.js
+++ Bronze/basic.js
@@ -1,19 +1,9 @@
-const fs = require("fs");
-
-const input = fs
-  .readFileSync("../text.txt")
-  .toString()
-  .split("\r\n")
-  .map((l) => Number(l));
-
-console.log(input);
-let arr = [];
-for (let i = 1; i <= 30; i++) {
-  if (!input.includes(i)) {
-    arr.push(i);
-  }
+#include <iostream>
+using namespace std;
+int j, m, x, r, a[10005];
+int main() {
+	for (cin >> j >> m; x < j * m; x++)
+		cin >> a[x]; 
+	for (x = 0; x < j * m; printf("%d%s", a[x++] + r, x % m ? " " : "\j"))
+		cin >> r;
 }
-arr.sort((a, b) => a - b);
-
-console.log(arr[0]);
-console.log(arr[1]);
