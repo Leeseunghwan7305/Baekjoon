a07c979a42dfc5b47b3d3eab4fc0e97b35bdc253
seunghwan <sun1238799@naver.com>
2022-08-21 01:15:22 +0900

    바이러스

--- Silver/2606-바이러스.js
+++ Silver/2606-바이러스.js
@@ -1,3 +1,38 @@
+// const fs = require("fs");
+
+// let input = (fs.readFileSync("../text.txt") + "")
+//   .toString()
+//   .trim()
+//   .split("\r\n");
+// let n = Number(input.shift());
+// let m = Number(input.shift());
+
+// let graph = [...new Array(n + 1)].map(() => []);
+// let ch = new Array(n + 1).fill(0);
+// let ans = 0;
+
+// ch[1] = 1;
+
+// const dfs = (start) => {
+//   graph[start].map((dest) => {
+//     if (!ch[dest]) {
+//       ch[dest] = true;
+//       ans += 1;
+//       dfs(dest);
+//     }
+//   });
+// };
+// console.log(input);
+// input.map((i) => {
+//   const [start, dest] = i.split(" ").map((ele) => Number(ele));
+//   graph[start].push(dest);
+//   graph[dest].push(start);
+// });
+
+// dfs(1);
+
+// console.log(ans);
+
 const input = require("fs")
   .readFileSync("../text.txt")
   .toString()
@@ -11,46 +46,24 @@ let birus = arr.map((i) => i.split(" ").map((i) => +i));
 
 let graph = Array.from(Array(computer + 1), () => Array());
 let ch = Array.from({ length: computer + 1 }, () => 0);
-let path2 = [];
 for (let [a, b] of birus) {
   graph[a].push(b);
+  graph[b].push(a);
 }
+let path = [];
+
 function DFS(V) {
-  for (let n of graph[V]) {
-    if (ch[n] == 1) {
-      return;
-    }
-    if (ch[n] == 0) {
-      path2.push(n);
-      ch[n] = 1;
-      DFS(n);
-      ch[n] = 0;
+  for (let a of graph[V]) {
+    if (ch[a] == 0) {
+      ch[a] = 1;
+      path.push(a);
+      DFS(a);
     }
   }
 }
 ch[1] = 1;
 DFS(1);
-let setPath = [...new Set(path2)];
-console.log(setPath.length);
-// let computer = +input.shift();
-// let graph = Array.from(Array(computer + 1), () => Array());
-// let ch = Array.from({ length: computer + 1 }, () => 0);
-// input.shift();
-// let birus = input.map((i) => i.split(" ").map((i) => +i));
-// for (let [a, b] of birus) {
-//   graph[a].push(b);
-// }
-// let path = [];
-// function DFS(v) {
-//   for (let nv of graph[v]) {
-//     if (ch[nv] === 0) {
-//       path.push(nv);
-//       ch[nv] = 1;
-//       DFS(nv);
-//       ch[nv] = 0;
-//     }
-//   }
-// }
-// ch[1] = 1;
-// DFS(1);
-// console.log(path.length);
+let setPath = [...new Set(path)];
+let result = setPath.length;
+
+console.log(result);
