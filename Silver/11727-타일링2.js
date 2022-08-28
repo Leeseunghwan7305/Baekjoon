c4f78e1542c12ed8fcf343d143b7d8bf6339851b
seunghwan <sun1238799@naver.com>
2022-08-28 17:55:50 +0900

    타일링2

---
+++ Silver/11727-타일링2.js
@@ -0,0 +1,16 @@
+let input = require("fs").readFileSync("../text.txt").toString().trim();
+
+let tmp = Array.from({ length: 1001 }, () => 0);
+
+tmp[0] = 1;
+tmp[1] = 3;
+tmp[2] = 5;
+
+for (let i = 3; i < tmp.length; i++) {
+  if (i % 2 == 0) {
+    tmp[i] = (tmp[i - 1] * 2 - 1) % 10007;
+  } else {
+    tmp[i] = (tmp[i - 1] * 2 + 1) % 10007;
+  }
+}
+console.log(tmp[input - 1]);
